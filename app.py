a6c630365e1a679134b24bcf15d6a1bf40096b7d
Bjarke Sørensen <bs@wasd.dk>
2020-04-13 21:22:22 +0200

    Extract game logic into separate module

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
-from player import Player
-from deck import Deck
+from game import Game
 
 
-game_deck = Deck()
-player1 = Player('Lars', game_deck)
-player2 = Player('Nils', game_deck)
-players = [player1, player2]
+def main():
+    game = Game('Adam', 'Jacob')
+    game.play()
 
-for player in players:
-    print(player)
 
-winner = False
-while not winner:
-    plays = [player.play() for player in players]
-    for i, player in enumerate(players):
-        print(f"{player.name} play a {plays[i]} ")
-
-    if plays[0] > plays[1]:
-        print(f"{players[0].name} wins the round!")
-        players[0].add_won_card(plays[0])
-    else:
-        print(f"{players[1].name} wins the round!")
-        players[1].add_won_card(plays[1])
-    if len(game_deck.cards) > 1:
-        for player in players:
-            player.draw(game_deck)
-
-    for player in players:
-        if player.winner():
-            print(f"{player.name} won these cards:")
-            print(f"{player.wins()}")
-            print(f"The winner is {player.name}")
-            winner = True
-
-    if not winner and len(game_deck.cards) == 0 and len(player1.hand) == 0:
-        winner = True
-        print(f"There is no winner. What even is the purpose?")
+if __name__ == '__main__':
+    main()
---
+++ game.py
@@ -0,0 +1,42 @@
+from player import Player
+from deck import Deck
+
+
+class Game:
+    def __init__(self, p1='Lars', p2='Nils'):
+        self.deck = Deck()
+        self.players = [Player(p1, self.deck), Player(p2, self.deck)]
+
+    def play(self):
+        for player in self.players:
+            print(player)
+
+        winner = False
+        while not winner:
+            plays = [player.play() for player in self.players]
+            for i, player in enumerate(self.players):
+                print(f"{player.name} play a {plays[i]} ")
+
+            if plays[0] > plays[1]:
+                print(f"{self.players[0].name} wins the round!")
+                self.players[0].add_won_card(plays[0])
+            elif plays[0] < plays[1]:
+                print(f"{self.players[0].name} wins the round!")
+                self.players[0].add_won_card(plays[0])
+            else:
+                print('It was a draw! Both cards are discarded.')
+
+            if len(self.deck.cards) > 1:
+                for player in self.players:
+                    player.draw(self.deck)
+
+            for player in self.players:
+                if player.winner():
+                    print(f"{player.name} won these cards:")
+                    print(f"{player.wins()}")
+                    print(f"The winner is {player.name}")
+                    winner = True
+
+            if not winner and len(self.deck.cards) == 0 and len(self.player[0].hand) == 0:
+                winner = True
+                print(f"There is no winner. What even is the purpose?")
